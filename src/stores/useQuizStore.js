20cde0dba4315b59ae9c6f5bb7ec13cda19f02f5
Ankush <ankushnitjsr@gmail.com>
2025-02-19 18:32:06 +0530

    feat: Update useQuizStore
    Add state for handling score on global level

--- src/stores/useQuizStore.js
+++ src/stores/useQuizStore.js
@@ -1,6 +1,11 @@
 import { create } from "zustand";
 
 export const useQuizStore = create((set) => ({
+  // for all questions
   questions: [],
   setQuestions: (questions) => set({ questions }),
+
+  // for total score
+  score: 0,
+  setScore: (score) => set({ score }),
 }));
