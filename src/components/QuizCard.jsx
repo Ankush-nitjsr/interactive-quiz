c22c2263f439b49131607d701091208a38350ca3
Ankush <ankushnitjsr@gmail.com>
2025-02-19 13:21:47 +0530

    feat: Update QuizCard component
    - Replace button element with OptionButton component for reusability & readability.
    - Add selectedOption and isCorrectOption props for handling button color

--- src/components/QuizCard.jsx
+++ src/components/QuizCard.jsx
@@ -1,20 +1,32 @@
+import OptionButton from "./ui/OptionButton";
+
 /* eslint-disable react/prop-types */
-const QuizCard = ({ question, options, handleAnswer, timeLeft }) => {
+const QuizCard = ({
+  question,
+  options,
+  handleAnswer,
+  timeLeft,
+  selectedOption,
+  isCorrectOption,
+}) => {
   return (
     <div className="p-4 text-center">
       <h2 className="text-2xl font-bold">{question}</h2>
-      <div className="mt-4">
+      <div className="mt-4 flex flex-col items-center">
         {options.map((option, index) => (
-          <button
+          <OptionButton
             key={index}
-            className="block w-full px-4 py-2 bg-blue-500 text-white rounded mt-2"
-            onClick={() => handleAnswer(option.isCorrect)}
-          >
-            {option.text}
-          </button>
+            selectedOption={selectedOption}
+            isCorrectOption={isCorrectOption}
+            option={option}
+            handleAnswer={handleAnswer}
+            timeLeft={timeLeft}
+          />
         ))}
       </div>
-      <p className="mt-4 text-red-500">Time left: {timeLeft}s</p>
+      {selectedOption === null && (
+        <p className="mt-4 text-red-500">Time left: {timeLeft} seconds</p>
+      )}
     </div>
   );
 };
