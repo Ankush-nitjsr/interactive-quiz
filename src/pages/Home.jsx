ba548ec88d7fb06f474e54ea9ddf6c9944cf4581
Ankush <ankushnitjsr@gmail.com>
2025-02-19 13:25:07 +0530

    style: Update Home component
    Update style of Home component for better UI

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,17 +2,18 @@ import { Link } from "react-router-dom";
 
 export default function Home() {
   return (
-    <div className="flex flex-col items-center justify-center h-screen">
-      <h1 className="text-3xl font-bold">
-        Welcome to the Interactive Quiz Platform
-      </h1>
+    <div className="flex flex-col items-center justify-center w-[50%] h-[50%] border border-gray-400 rounded-3xl">
+      <h1 className="text-3xl font-bold">Welcome to the Interactive Quiz</h1>
       <div className="mt-6">
-        <Link to="/quiz" className="px-4 py-2 bg-blue-500 text-white rounded">
+        <Link
+          to="/quiz"
+          className="px-4 py-2 bg-blue-500 hover:bg-blue-400 text-white rounded "
+        >
           Start Quiz
         </Link>
         <Link
           to="/history"
-          className="ml-4 px-4 py-2 bg-gray-500 text-white rounded"
+          className="ml-4 px-4 py-2 bg-gray-500 hover:bg-gray-400 text-white rounded"
         >
           View History
         </Link>
