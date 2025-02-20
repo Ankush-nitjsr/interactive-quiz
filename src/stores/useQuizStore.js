d10e2c263dde583aba6485f9fa932e68c8da3ce0
Ankush <ankushnitjsr@gmail.com>
2025-02-20 13:58:07 +0530

    chore: Update useQuizStore
    - Add Summary and meaningful comments

--- src/stores/useQuizStore.js
+++ src/stores/useQuizStore.js
@@ -1,5 +1,18 @@
 import { create } from "zustand";
 
+/**
+ * useQuizStore - A Zustand store for managing quiz state.
+ *
+ * @function
+ * @param {function} set - Zustand's set function to update the store state.
+ *
+ * @property {Array} questions - An array to hold all quiz questions.
+ * @property {function} setQuestions - A function to set the quiz questions.
+ *
+ * @property {number} score - The total score of the quiz.
+ * @property {function} setScore - A function to set the total score.
+ */
+
 export const useQuizStore = create((set) => ({
   // for all questions
   questions: [],
