b5fea455e996976727c71ea4fbf2298be373134b
Ankush <ankushnitjsr@gmail.com>
2025-02-20 13:35:54 +0530

    chore: Update Header component
    - Add PropType for all props validation
    - Add Summary and meaningful comments

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,13 +1,46 @@
-/* eslint-disable react/prop-types */
+import { Link } from "react-router-dom";
 import Timer from "../Timer";
+import PropType from "prop-types";
+
+/**
+ * Header component that displays the navigation bar with links and a timer.
+ *
+ * This component provides navigation links to the home and history pages,
+ * along with a timer to track the remaining time for the quiz.
+ *
+ * @component
+ * @param {Object} props - The component props.
+ * @param {number} props.timeLeft - The remaining time for the quiz.
+ * @param {function} props.setTimeLeft - Function to update the remaining time.
+ * @returns {JSX.Element} The rendered Header component.
+ */
 
 export default function Header({ timeLeft, setTimeLeft }) {
   return (
-    <div className="flex justify-between items-center">
-      <h1 className="text-gray-800 text-2xl font-semibold">
-        Interactive Quiz Application
-      </h1>
-      <Timer timeLeft={timeLeft} setTimeLeft={setTimeLeft} />
-    </div>
+    <header className="p-4 bg-white shadow-md">
+      {/* Navigation Bar */}
+      <nav className="flex justify-between items-center max-w-4xl mx-auto">
+        {/* Home Link */}
+        <Link to="/" className="text-xl font-bold hover:text-purple-600">
+          Interactive Quiz
+        </Link>
+
+        {/* History Link */}
+        <Link
+          to="/history"
+          className="hover:underline text-purple-600 text-lg font-semibold"
+        >
+          History
+        </Link>
+
+        {/* Quiz Timer */}
+        <Timer timeLeft={timeLeft} setTimeLeft={setTimeLeft} />
+      </nav>
+    </header>
   );
 }
+
+Header.propTypes = {
+  timeLeft: PropType.number.isRequired,
+  setTimeLeft: PropType.func.isRequired,
+};
