314b66318a8f043c4a0aa1d10ff26d9164c6d7c5
Ankush <ankushnitjsr@gmail.com>
2025-02-20 13:46:02 +0530

    chore: Update Scoreboard component
    - Add Summary and meaningful comments

--- src/components/Scoreboard.jsx
+++ src/components/Scoreboard.jsx
@@ -2,6 +2,20 @@ import { useNavigate } from "react-router-dom";
 import { useQuizStore } from "../stores/useQuizStore";
 import calculateTotalScore from "../utils/calculateTotalScore";
 
+/**
+ * Scoreboard component displays the quiz results including the total score,
+ * total questions, correct answers, and incorrect answers. It also provides
+ * an option to reset the score and try the quiz again.
+ *
+ * @component
+ * @example
+ * return (
+ *   <Scoreboard />
+ * )
+ *
+ * @returns {JSX.Element} The rendered Scoreboard component.
+ */
+
 export default function Scoreboard() {
   // Access the questions and score from the store
   const { questions, score, setScore } = useQuizStore();
@@ -23,8 +37,8 @@ export default function Scoreboard() {
     <div className="p-6 w-[40%] h-[50%] space-y-6 bg-gradient-to-b from-gray-100 to-purple-200 rounded-lg shadow-lg text-center text-white">
       <h1 className="text-4xl font-bold mb-4 text-fuchsia-800">Quiz Results</h1>
       <div>
-        <h2 className="text-3xl font-bold mb-4 text-purple-400">Your Score</h2>
-        <p className="text-6xl font-semibold text-purple-400">{score}</p>
+        <h2 className="text-3xl font-bold mb-4 text-purple-600">Your Score</h2>
+        <p className="text-6xl font-semibold text-purple-600">{score}</p>
       </div>
       <div className="flex flex-col w-[42%] mx-auto">
         <div className="flex justify-between items-center space-x-2 px-8">
@@ -45,7 +59,7 @@ export default function Scoreboard() {
         </div>
       </div>
       <button
-        className="border border-black bg-purple-400 rounded-2xl px-4 py-2 mt-4 hover:bg-purple-300"
+        className="bg-purple-500 rounded-2xl px-4 py-2 mt-4 hover:bg-purple-600 transition-all duration-300 ease-in-out"
         onClick={handleTryAgain}
       >
         Try again
